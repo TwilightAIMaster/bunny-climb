921477ebb9ad55110d932524c1b8532ffc8a4db0
Twilight <karlo09.kl@gmail.com>
2026-04-07 00:11:39 -0700

    Fix: add pod install to cloud build

--- ios/App/App/ci_scripts/ci_post_clone.sh
+++ ios/App/App/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# 1. Reach the root (Updated for the App/App path)
+# 1. Reach the root
 cd ../../../../../
 
 # 2. Clean up
@@ -9,10 +9,13 @@ rm -f config.xml
 rm -rf www
 rm -rf public
 
-# 3. Build
+# 3. Build web assets
 npm install
 npm run build
 
-# 4. Sync
+# 4. Sync and install Native Pods
 npx cap sync ios
 
+# Move into the iOS folder relative to the ROOT
+cd ios/App
+pod install
